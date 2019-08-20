9e7477361d96d7529465a80a5b6b3edd1ec29ae2
mlmiller4 <mlmiller4@gmail.com>
2019-08-20 16:13:31 -0400

    Fixed problem with getting JSON from the top 10 actors dataframe.

--- flask_app_IMDB.py
+++ flask_app_IMDB.py
@@ -7,113 +7,139 @@ import json
 app = Flask(__name__)
 api = Api(app)
 
-actors = [
-    {
-        "last name": "Ford",
-        "full name": "Harrison Ford",
-        "facebook likes": 11000
-    },
-    {
-        "last name": "Hanks",
-        "full name": "Tom Hanks",
-        "facebook likes": 15000
-    },
-    {
-        "last name": "Lawrence",
-        "full name": "Jennifer Lawrence",
-        "facebook likes": 34000
-    },
-    {
-        "last name": "Cruise",
-        "full name": "Tom Cruise",
-        "facebook likes": 10000
-    },
-    {
-        "last name": "Pattinson",
-        "full name": "Robert Pattinson",
-        "facebook likes": 21000
-    },
-    {
-        "last name": "Cooper",
-        "full name": "Bradley Cooper",
-        "facebook likes": 14000
-    },
-    {
-        "last name": "Hemsworth",
-        "full name": "Chris Hemsworth",
-        "facebook likes": 26000
-    },
-    {
-        "last name": "Williams",
-        "full name": "Robin Williams",
-        "facebook likes": 49000
-    },
-    {
-        "last name": "Simmons",
-        "full name": "J.K. Simmons",
-        "facebook likes": 24000
-    },
-    {
-        "last name": "DiCaprio",
-        "full name": "Leonardo DiCaprio",
-        "facebook likes": 29000
-    }
-]
+# Load the movie_metadata.csv and get the top 10 most profitable actors
+appDF = pd.read_csv("movie_metadata.csv")
+appDF = IMDB.calculateProfit(appDF)
+actorsDF = IMDB.get_columns(appDF, ['actor_1_name','profit'])
+top10actors = IMDB.top10(actorsDF, 'actor_1_name', 'profit', 'sum')
+
+del top10actors['sum_profit']               # don't need profit info
+top10actors.columns = ['name']
+top10actors['facebook likes'] = 0
+top10actors['unspacedName'] = top10actors['name'].str.replace(' ','')   # Create column for actor name with no spaces
+
+# Add in number of facebook likes data
+for index, row in top10actors.iterrows():
+    matchingRow = appDF[appDF['actor_1_name'] == row['name']].iloc[0]
+    top10actors.at[index, 'facebook likes'] = matchingRow['actor_1_facebook_likes']
+
+top10actors = top10actors.reset_index(drop=True)    # reset index
+
+# Data must be formatted as a series of dictionaries where 'name','facebook likes', and 'unspacedName' are keys
+actors = top10actors.to_dict('records')
+
+
+''' test data - not used when accessing data through IMDB functions '''
+# actors = [
+#     {
+#         "last name": "Ford",
+#         "full name": "Harrison Ford",
+#         "facebook likes": 11000
+#     },
+#     {
+#         "last name": "Hanks",
+#         "full name": "Tom Hanks",
+#         "facebook likes": 15000
+#     },
+#     {
+#         "last name": "Lawrence",
+#         "full name": "Jennifer Lawrence",
+#         "facebook likes": 34000
+#     },
+#     {
+#         "last name": "Cruise",
+#         "full name": "Tom Cruise",
+#         "facebook likes": 10000
+#     },
+#     {
+#         "last name": "Pattinson",
+#         "full name": "Robert Pattinson",
+#         "facebook likes": 21000
+#     },
+#     {
+#         "last name": "Cooper",
+#         "full name": "Bradley Cooper",
+#         "facebook likes": 14000
+#     },
+#     {
+#         "last name": "Hemsworth",
+#         "full name": "Chris Hemsworth",
+#         "facebook likes": 26000
+#     },
+#     {
+#         "last name": "Williams",
+#         "full name": "Robin Williams",
+#         "facebook likes": 49000
+#     },
+#     {
+#         "last name": "Simmons",
+#         "full name": "J.K. Simmons",
+#         "facebook likes": 24000
+#     },
+#     {
+#         "last name": "DiCaprio",
+#         "full name": "Leonardo DiCaprio",
+#         "facebook likes": 29000
+#     }
+# ]
 
 class Actor(Resource):
 
-    def get(self, lastName):
+    def get(self, unspacedName):
         for actor in actors:
-            if(lastName == actor['last name']):
+            if unspacedName == actor['unspacedName']:
                 return actor, 200
-        return "Actor not found", 404
+        return 'Actor not found', 404
 
-    def post(self, lastName):
+
+    def post(self, unspacedName):
         parser = reqparse.RequestParser()
-        parser.add_argument("full name")
+        parser.add_argument("name")
         parser.add_argument("facebook likes")
         args = parser.parse_args()
 
         for actor in actors:
-            if(lastName == actor["last name"]):
-                return "Actor with name {} already exists".format(lastName), 400
+            if(unspacedName == actor['unspacedName']):
+                return "Actor with the name {} already exists".format(actor['name']), 400
 
         actor = {
-            "last name": lastName,
-            "full name": args["full name"],
+            "unspacedName": unspacedName,
+            "name": args["name"],
             "facebook likes": args["facebook likes"]
         }
 
         actors.append(actor)
         return actor, 201
 
-    def put(self, lastName):
+    def put(self, unspacedName):
         parser = reqparse.RequestParser()
-        parser.add_argument("full name")
+        parser.add_argument("name")
         parser.add_argument("facebook likes")
         args = parser.parse_args()
 
+        # Update info if the actor is already present
         for actor in actors:
-            if(lastName == actor["last name"]):
-                actor["full name"] = args["full name"]
+            if(unspacedName == actor["unspacedName"]):
+                actor["name"] = args["name"]
                 actor["facebook likes"] = args["facebook likes"]
                 return actor, 200
 
+        # If actor is not already present, append new actor info
         actor = {
-            "last name": lastName,
-            "full name": args["full name"],
+            "unspacedName": unspacedName,
+            "name": args["name"],
             "facebook likes": args["facebook likes"]
         }
 
         actors.append(actor)
         return actor, 201
 
-    def delete(self, lastName):
+    def delete(self, unspacedName):
         global actors
-        actors = [actor for actor in actors if actor['last name'] != lastName]
-        return "{} is deleted".format(lastName), 200
+        actors = [actor for actor in actors if actor['unspacedName'] != unspacedName]
+        return "{} is deleted".format(unspacedName), 200
 
-api.add_resource(Actor, "/actor/<string:lastName>")
+api.add_resource(Actor, "/actor/<string:unspacedName>")
 app.run(debug=True)
 
 
